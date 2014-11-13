b4fe9ab64cb1e6888b0634912b711e9c260528dd
Glavin Wiechert <glavin.wiechert@gmail.com>
2014-11-12 21:07:04 -0400

    See #4. Add File API to server.

--- webapp/app/scripts/main.js
+++ webapp/app/scripts/main.js
@@ -8,11 +8,23 @@ var i = 0; // Click counter
 $continueBtn.on('click', function(event) {
   console.log("Click event received!");
   i++; // i += 1; i = i + 1;
-  p.text("I have been clicked "+i+" times.");
+  $p.text("I have been clicked "+i+" times.");
 });
 
 var $submitBtn = $('button.get-file-contents-btn');
 var $filename = $('input[name="filename"]');
 $submitBtn.on('click', function(event) {
-  console.log($filename.val());
+  // console.log($filename.val());
+  var server = "http://localhost:3000/";
+  var apiPath = "file/"
+  var filename = $filename.val();
+  $.ajax(server+apiPath+filename)
+  .done(function(data) { 
+    console.log(data);
+    $p.text(data);
+  })
+  .fail(function(err) { 
+    console.log(err); 
+  })
 });
+
--- webapp/server/index.js
+++ webapp/server/index.js
@@ -1,8 +1,37 @@
 var express = require('express');
-var app = express();
+var fs = require('fs');
+var path = require('path');
+var cors = require('cors');
 
-app.get('/', function(req, res){
-  res.send('hello world');
+// Create server
+var app = express();
+// Fix CORS
+app.use(cors());
+
+var count = 0; // # of visitors
+app.get('/', function(request, response) {
+  count++;
+  response.send('This page has been requested '+count+' times!');
 });
 
-app.listen(3000);
+app.get('/file/:filename', function (req, res) {
+  var filename = req.params.filename;
+  var filepath = path.resolve(__dirname, "data", filename);
+  console.log(filepath);
+
+  // Read file in ASYNC (non blocking I/O)
+  fs.readFile(filepath, function (err, data) {
+    if (err) {
+      // throw err;
+      res.send(err.message);
+    } else {
+      res.send(data);
+    }
+  });
+
+});
+
+var port = 3000;
+app.listen(port, function() {
+  console.log('Server is listening on port '+port);
+});
